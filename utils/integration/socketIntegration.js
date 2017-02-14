dfc18c95414aabe6eeacf5f613d9c42f2abaa7c0
Shefik <caner.shefik@capgemini.com>
2017-02-14 14:04:27 +0000

    Component to Send Socket Notifications

---
+++ utils/integration/socketIntegration.js
@@ -0,0 +1,20 @@
+/**
+ * Created by CSHEFIK on 14/02/2017.
+ */
+
+var socketIO;
+
+var initialise = function (server) {
+  socketIO = require('socket.io')(server);
+
+  socketIO.on('connection', function (socket) {
+    socket.on('join', function(room) {
+      socket.join(room);
+    });});
+}
+
+var sendClsimUpdate = function (claimData,roomID){
+  socketIO.to(roomID).emit('claim update', claimData)
+}
+
+
