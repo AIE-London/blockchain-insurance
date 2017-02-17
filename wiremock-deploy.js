c09752d3fdc6a11056d8a0e247926b81b413c779
Dan Cotton <dan.cotton@capgemini.com>
2017-02-17 14:39:23 +0000

    improved wiremock deployment logging

--- wiremock-deploy.js
+++ wiremock-deploy.js
@@ -14,14 +14,29 @@ function readFiles(){
 }
 
 function run() {
-  readFiles().forEach(function (mapping) {
+  var count = {
+    all: 0,
+    success: 0
+  };
+  var files = readFiles();
+  files.forEach(function (mapping) {
     request('POST', _mappingUrl, {
       headers: {
         'content-type': 'application/json'
       },
       body: JSON.stringify(mapping)
     }).then(function (response) {
-      console.log(response.statusCode);
+      count.all++;
+      if (response.statusCode >= 200 && response.statusCode < 3000) {
+        count.success++;
+      } else {
+        console.error("Failed for mapping: \n \n " + JSON.stringify(mapping) + " \n \n With Response: \n \n" +response);
+      }
+      if (count.all === files.length - 1 && count.success === files.length - 1) {
+        console.log("Successful Deployment");
+      } else if (count.all === files.length - 1) {
+        console.error("Unsuccessful Deployment");
+      }
     });
   });
 }
