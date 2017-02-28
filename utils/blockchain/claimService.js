3dfa664f7f2cd98730fce8260da0326c52cd0f73
Allen <scott.allen@capgemini.com>
2017-02-28 10:39:21 +0000

    Added args to create claim invoke request and updated post schema to reflect available claim types

--- utils/blockchain/claimService.js
+++ utils/blockchain/claimService.js
@@ -3,7 +3,21 @@ var config = require('config');
 
 var raiseClaim = function(claim, username, callback){
   // We can be assured the claim has the relevant fields because of schema validaiton ont he endpoint
+
+  // Map UI Types to Blockchain Types
+  if (claim.type.toLowerCase() === "multiple party"){
+    claim.type = "multiple_parties";
+  } else if (claim.type.toLowerCase() === "Single Party"){
+    claim.type = "single_party";
+  }
+
   var args = [claim.relatedPolicy, claim.description, claim.incidentDate, claim.type];
+
+  // Only required for multi-party; added here rather than within multi-party if so can push to args
+  if (claim.otherPartyReg){
+    args.push(claim.otherPartyReg, (claim.atFault === true))
+  };
+
   blockchainService.invoke("createClaim", args, username, callback);
 };
 
